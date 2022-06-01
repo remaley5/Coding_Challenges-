b2cca1fc6bd359c55ce91456af1c28d5de3a6fb5
remaley5 <swillson@alumni.risd.edu>
2022-06-01 15:47:51 -0700

    finish add_eventing

--- javascript-challenges-master/add-eventing/add-eventing.js
+++ javascript-challenges-master/add-eventing/add-eventing.js
@@ -1,4 +1,24 @@
 const addEventing = function (obj) {
+    var events = {};
+
+    obj.on = function(event, callback) {
+        if(!events[event]) {
+            events[event] = [callback];
+        } else {
+            events[event].push(callback);
+        }
+    }
+
+    obj.trigger = function(event) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        if(events[event]) {
+            events[event].forEach(function(cb) {
+                cb.apply(null, args);
+            });
+        }
+    }
+
+    return obj;
 }
 
-module.exports = addEventing
+module.exports = addEventing;
