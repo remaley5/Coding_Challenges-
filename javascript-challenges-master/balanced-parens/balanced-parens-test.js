53cdf5ad2892f6561566199dcff2bcaf61b4568f
remaley5 <swillson@alumni.risd.edu>
2022-06-01 18:01:50 -0700

    removed skip on balanced params test, 12 passing

--- javascript-challenges-master/balanced-parens/balanced-parens-test.js
+++ javascript-challenges-master/balanced-parens/balanced-parens-test.js
@@ -10,28 +10,28 @@ test('it matches braces when only one pair of one type exists', assert => {
   assert.end()
 })
 
-test.skip('it matches braces when multiple pairs of one type exist', assert => {
+test('it matches braces when multiple pairs of one type exist', assert => {
   assert.equal(parensAreBalanced('((()))'), true)
   assert.equal(parensAreBalanced('((())'), false)
 
   assert.end()
 })
 
-test.skip('it matches braces when multiple pairs of multiple types exist', assert => {
+test('it matches braces when multiple pairs of multiple types exist', assert => {
   assert.equal(parensAreBalanced('{([])}'), true)
   assert.equal(parensAreBalanced('{([]}'), false)
 
   assert.end()
 })
 
-test.skip('it works correctly when no braces exist', assert => {
+test('it works correctly when no braces exist', assert => {
   assert.equal(parensAreBalanced(''), true)
   assert.equal(parensAreBalanced('hello world'), true)
 
   assert.end()
 })
 
-test.skip('it matches braces when braces and other characters exist', assert => {
+test('it matches braces when braces and other characters exist', assert => {
   assert.equal(parensAreBalanced('function () { console.log(["cat", "dog"]) }'), true)
   assert.equal(parensAreBalanced('function ( { console.log(["cat", "dog"]) }'), false)
 
