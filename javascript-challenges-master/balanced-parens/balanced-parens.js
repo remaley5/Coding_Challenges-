8c15cacc90e47e4ab35a9753f5c167969047bf68
remaley5 <swillson@alumni.risd.edu>
2022-06-01 17:34:01 -0700

    refactored balanced-params, still passing

--- javascript-challenges-master/balanced-parens/balanced-parens.js
+++ javascript-challenges-master/balanced-parens/balanced-parens.js
@@ -1,42 +1,35 @@
 const parensAreBalanced = (input) => {
-  var tracking = [];
+  var trackOpenParams = [];
 
   let openParams = '{[(';
   let closeParams = '}])';
   let current;
-  let lastParam;
 
 
   // Loop through string
   for(let i = 0; i < input.length; i++) {
     current = input[i];
     
-    if(openParams.includes(current)) {
-      // If forward param add to tracking
-      tracking.unshift(current);
-    
-      // else reverse param is found, check the last param in tracking
-    } else if(closeParams.includes(current)) {
-        lastParam = tracking[0];
-        if(
-          (current === '}' && lastParam === '{') ||
-          (current === ')' && lastParam === '(') ||
-          (current === ']' && lastParam === '[') 
-          ) {
-            // if they are a match remove match from tracking
-            tracking.shift();
-          } else {
-            // If not a match return false
-          return false;
-        }
+    if(!(closeParams.includes(current) || openParams.includes(current))) {
+      // ignore if it's not a param
+      continue;
+    } else if (openParams.includes(current)) {
+      // track open params
+      trackOpenParams.unshift(current);
+    } else if ( openParams.indexOf(trackOpenParams[0])  === closeParams.indexOf(current)) {
+      // removed matched params from tracking
+      trackOpenParams.shift();
+    } else {
+      // return false if unmatched is found
+      return false;
     }
-    // Otherwise it's not a param and can be ignored
   }
   
-  // if it makes it through check for remaining params
-  if(tracking.length === 0) {
+  if(trackOpenParams.length === 0) {
+    // return true if all params were matched
     return true;
   } else {
+    // return false if unmatched params remain
     return false;
   }
 }
