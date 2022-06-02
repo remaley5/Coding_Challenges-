377128729516d74271473c33b34382475248a795
remaley5 <swillson@alumni.risd.edu>
2022-06-01 18:36:16 -0700

    most-reccurrent, refactored with recursion, passing

--- javascript-challenges-master/number-frequency/most-recurrent.js
+++ javascript-challenges-master/number-frequency/most-recurrent.js
@@ -1,25 +1,64 @@
-const mostRecurrent = (set) => {
-  let lastMax = 0;
-  nextArray = set;
-  let max;
-  let mostRec;
-  let trackArray = [];
-  set.forEach(current => {
-    max = 0;
-    set.forEach(second => {
-      if(second === current) {
-        max ++;
-      } else {
-        trackArray.push(second);
-      }
-    });
-    if(max > lastMax) {
-      mostRec = current;
+//----------------------
+// Recursion
+//----------------------
+
+function recurse(set, lastMax, num) {
+  if(set.length === 0) {
+    return num;
+  } 
+
+  let current = set[0];
+  let max = 0;
+  let arr = [];
+
+  set.forEach(second => {
+    if(current === second) {
+      max ++;
+    } else {
+      arr.push(second);
     }
-    nextArray = trackArray;
-    trackArray = [];
   });
-  return mostRec;
+  
+  if(lastMax >= max) {
+    // reset to last
+    current = num;
+    max = lastMax;
+  }
+
+  return recurse(arr, max, current);
 }
+
+const mostRecurrent = (set) => {
+  return recurse(set, 0, null);
+}
+
+
+//----------------------------
+// With Nested Loop
+//----------------------------
+
+// const mostRecurrent = (set) => {
+//   let lastMax = 0;
+//   nextArray = set;
+//   let max;
+//   let mostRec;
+//   let trackArray = [];
+//   set.forEach(current => {
+//     max = 0;
+//     set.forEach(second => {
+//       if(second === current) {
+//         max ++;
+//       } else {
+//         trackArray.push(second);
+//       }
+//     });
+//     if(max > lastMax) {
+//       mostRec = current;
+//     }
+//     nextArray = trackArray;
+//     trackArray = [];
+//   });
+//   return mostRec;
+// }
 
 module.exports = mostRecurrent
