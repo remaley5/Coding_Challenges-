c3be0a32b44a05d1c4a8f5da0b427fc97a988f12
remaley5 <swillson@alumni.risd.edu>
2022-06-01 18:13:56 -0700

    most-current, passing, reduce second array

--- javascript-challenges-master/number-frequency/most-recurrent.js
+++ javascript-challenges-master/number-frequency/most-recurrent.js
@@ -1,17 +1,23 @@
 const mostRecurrent = (set) => {
   let lastMax = 0;
+  nextArray = set;
   let max;
   let mostRec;
+  let trackArray = [];
   set.forEach(current => {
     max = 0;
     set.forEach(second => {
       if(second === current) {
         max ++;
+      } else {
+        trackArray.push(second);
       }
     });
     if(max > lastMax) {
       mostRec = current;
     }
+    nextArray = trackArray;
+    trackArray = [];
   });
   return mostRec;
 }
