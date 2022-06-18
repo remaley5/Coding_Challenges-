c38b807739f899838fda4465206ccd103c3989e6
remaley5 <swillson@alumni.risd.edu>
2022-06-17 19:53:03 -0700

    removed console log

--- javascript-challenges-master/object-diff/object-diff.js
+++ javascript-challenges-master/object-diff/object-diff.js
@@ -4,7 +4,6 @@ const diff = (oldCode, newCode )  => {
   let differences = [];
 
   const pushRemaining = (value, lastKey) => {
-    console.log('PR: lastKey: ', lastKey);
     if(typeof value === "object" && value !== null) {
       console.log('- calling again');
       for(key in value) {
@@ -16,15 +15,12 @@ const diff = (oldCode, newCode )  => {
   }
 
   const objectDiff = (oldVal, newVal, lastKey) => {
-    console.log('oD: LastKey: ', lastKey);
     if(oldVal === newVal) {
       return;
     } else if((!newVal || !(typeof newVal === "object" && newVal !== null))) {
-      console.log(' - calling pR')
       pushRemaining(oldVal, lastKey);
     } else if(typeof oldVal === "object" && oldVal !== null) {
       for(key in oldVal) {
-        console.log('- calling again', lastKey, key);
         objectDiff(oldVal[key], newVal[key], lastKey ? lastKey + '.' + key : key);
       }
     } 
